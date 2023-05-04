4bfa899a6df13731cc70f6effa60022a64026cb3
yusivacova <sivacovayulia@gmail.com>
2023-05-04 22:38:34 +0300

    feat: add solution fifth task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
+
+function glueStyleCssFile() {
+    const output = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+
+    fsPromises.readdir((path.join(__dirname, 'styles')), {
+        withFileTypes: true,
+    }).then(files => {
+        files.forEach(file => {
+            const filePath = path.join(__dirname, 'styles', file.name);
+            const fileName = path.basename(filePath);
+            const fileExt = path.extname(filePath);
+            if (fileExt === '.css') {
+                const input = fs.createReadStream(filePath, 'utf-8');
+                input.on('data', chunk => output.write(chunk));
+            }
+        })
+    })
+}
+
+glueStyleCssFile();
